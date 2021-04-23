fd99a05c71127f896dfaf1d74e064106a043a842
AbdulN <abdalaziznezar@gmail.com>
2021-04-22 18:33:50 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,12 @@ BOT_NAME = "Altec" #Change this into whatever you want.
 
 bot = commands.Bot(command_prefix="!")
 
+with open("config.json", "r") as f:
+    config = json.load(f)
+
 @bot.event
 async def on_ready():
     print(f"{BOT_NAME} is online!")
 
 
-bot.run(TOKEN)
+bot.run(config["token"], reconnect=True)
